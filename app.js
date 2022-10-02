6657359993ee380ecff235fc383fec8f1dfe8d57
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-10-02 18:32:08 +0530

    footer added and styled

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-var serverURl = "https://api.funtranslations.com/translate/chef.json"
+var btnTranslate = document.querySelector("#btn-translate");
+//console.log(btnTranslate);
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector(".output");
+
+var serverURL = "https://api.funtranslations.com/translate/chef.json"
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something went wrong with server ! try after some time")
+}
+
+
+
+function getTranslatorURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function clickHandler() {
+    var inputText = txtInput.value
+
+    fetch(getTranslatorURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
